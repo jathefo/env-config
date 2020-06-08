5e2ca48a068edf9c56bbb671a3e7c2efeaa662a4
MZNight <jathefo@126.com>
2020-06-08 17:31:12 +0800

    Update cscope, and vimrc for status bar.

--- bin/cs.sh
+++ bin/cs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-find . -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.java" > cscope.files
+find $PWD -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.java" > cscope.files
 cscope -bkq -i cscope.files
 ctags -R 
